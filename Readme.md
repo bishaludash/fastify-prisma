e9bc1303dd7f3425d204c95dadc8bbc03a73da68
bishaludash <supertramp.bishal@gmail.com>
2024-06-03 10:16:22 +0200

    Fastify Project setup

---
+++ Readme.md
@@ -0,0 +1,5 @@
+### Project Setup
+
+- yarn add @prisma/client fastify fastify-zod zod-to-json-schema fastify-jwt
+- yarn add ts-node-dev typescript @types/node --dev
+- [Typescript Installation Guide](https://www.digitalocean.com/community/tutorials/typescript-new-project)
---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
+});
+
+fastify.get("/healthcheck", async (req, res) => {
+  res.send({ status: "OK" });
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000, host: "0.0.0.0" });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
