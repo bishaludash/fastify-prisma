2ccb04be3458628a3ae651863e294ee25feef532
bishaludash <supertramp.bishal@gmail.com>
2024-06-07 12:09:19 +0200

    implement fastify-zod to parse request payloads

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import userRoutes from "./modules/users/user.route";
+import { userSchemas } from "./modules/users/user.schema";
 
 const fastify = Fastify({
   logger: true,
@@ -12,7 +13,11 @@ fastify.get("/healthcheck", async (req, res) => {
 });
 
 // Run the server!
-const start = async () => {
+const main = async () => {
+  for (const schema of userSchemas) {
+    fastify.addSchema(schema);
+  }
+
   try {
     fastify.register(userRoutes, { prefix: "api/users" });
     await fastify.listen({ port: 3000, host: "0.0.0.0" });
@@ -21,4 +26,4 @@ const start = async () => {
     process.exit(1);
   }
 };
-start();
+main();
